4eeb3d3772ce7a5671982948ef21f3f21f6a8fe4
Arthur Rob <arthur.rob.dev@gmail.com>
2025-06-24 12:18:03 +0200

    init(project): setup project

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { NewsletterModule } from './newsletter/newsletter.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true, // rend ConfigModule disponible dans toute l'app
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get<string>('DB_HOST'),
+                port: parseInt(configService.get<string>('DB_PORT') || '3306', 10),
+                username:  configService.get<string>('DB_USERNAME'),
+                password:  configService.get<string>('DB_PASSWORD'),
+                database:  configService.get<string>('DB_NAME'),
+                autoLoadEntities: true,
+                synchronize: configService.get<string>('NODE_ENV') !== 'production',
+            })
+        }),
+        NewsletterModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
